bcba92013034055cd13c09e6536265a81137009b
TobyDarmawan <36318985+TobyDarmawan@users.noreply.github.com>
2018-02-10 14:04:27 +0700

    Add files via upload

---
+++ masterList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fishackathon;
+
+/**
+ *
+ * @author 46158
+ */
+public class masterList {
+    public static void main(String[] args) {
+        String choice;
+
+    }
+    public void sort(){
+        
+    }
+    public void filter(){
+        
+    }
+    public void search(String word){
+        
+    }
+}
